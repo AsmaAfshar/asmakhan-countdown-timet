4394613616e82019f054c471230cc60a390f9b68
Asma Afshar <161859558+AsmaAfshar@users.noreply.github.com>
2024-05-23 21:12:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//Countdown Timer Project
+//In this project, you will build a countdown timer using the date module.
+//date module install cammand
+//npm install date-fns
+// start coding: step # 1
+import { differenceInSeconds } from "date-fns";
+// funcrtion for count down second
+function* countdownTimer(second) {
+    // while loop
+    while (second > 0) {
+        yield second;
+        second--;
+    }
+}
+// step 2: counter initialization
+let timeIterator = countdownTimer(10);
+// function to create a count down timer
+function displayCountdown() {
+    // value below 10
+    let result = timeIterator.next();
+    // if else condition apply
+    if (!result.done) {
+        //current date and time calls
+        const now = new Date();
+        // calculate  minutes in seconds in time
+        const countdownTime = new Date(now.getTime() + (result.value * 1000));
+        //calculate remaining seconds in time
+        const remainingSeconds = differenceInSeconds(countdownTime, now);
+        console.log(`Remaining Seconds: ${remainingSeconds}`);
+        setTimeout(displayCountdown, 1000); // 1 seconds is equal to 1000 ms
+    }
+    else {
+        //display result contdown complete
+        console.log("Countdown Complete!");
+    }
+}
+// invoke 
+displayCountdown();
